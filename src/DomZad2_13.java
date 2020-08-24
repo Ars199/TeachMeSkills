60d8d9b69927f19cefaa906d72f219d48f9fce70
Arseniy Yanson <arseniy.yanson@realizeideas.net>
2020-08-24 14:55:15 +0300

    DomZad2.13_2

--- src/DomZad2_13.java
+++ src/DomZad2_13.java
@@ -4,9 +4,17 @@ public class DomZad2_13 {
         //Найдите все четные от 2 до 100
 
 
-        for (int i = 2; i <= 100; i++) {
+        /*for (int i = 2; i <= 100; i++) {
             if (i % 2 == 0) {
                 System.out.println("Четное. " + i);
+            }
+        }
+    }
+}*/
+
+        for (int i = 2; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Нечетное. " + i);
             }
         }
     }
