dedfd6f2bee0bec8f6901f894a9e78f8fa8e6e72
Arseniy Yanson <arseniy.yanson@realizeideas.net>
2020-08-25 15:52:23 +0300

    While>Practice

---
+++ src/PracticeWhile.java
@@ -0,0 +1,36 @@
+
+public class PracticeWhile {
+    public static void main(String[] args) {
+        int x1 = 1;
+        System.out.println("Перед началом цикла");
+        while (x1 < 4) {
+            System.out.println("Внутри цикла");
+            System.out.println("Значение Х равно " + x1);
+            x1 += 1;
+        }
+        System.out.println("-------второй пример-----------");
+
+        int x = 1;
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
