e1a5412f11ae055d4a93a8a2dfb9292e0000dd7f
Ars199 <43823568+Ars199@users.noreply.github.com>
2020-08-26 16:35:55 +0300

    massive Initial

--- src/Practice7massiveInit.java
+++ src/Practice7massiveInit.java
@@ -2,22 +2,25 @@
 public class Practice7massiveInit {
 
     public static void main(String[] args) {
-        double[] myList = {1.9, 2.9, 3.4, 3.5};
+        double[] myList = {1.9, 2.9, 3.5, 3.3};
 
         // Вывести на экран все элементы массива
         for (int i = 0; i < myList.length; i++) {
             System.out.println(myList[i] + " ");
         }
+        System.out.println("Обращение ко второму элементу по индексу: " + myList[1]);
         // Сумма элементов массива
         double total = 0;
         for (int i = 0; i < myList.length; i++) {
-            total += myList[i];
+            total = total + myList[i];
         }
         System.out.println("Сумма чисел массива: " + total);
         // Нахождение среди элементов массива наибольшего
         double max = myList[0];
         for (int i = 1; i < myList.length; i++) {
-            if (myList[i] > max) max = myList[i];
+            if (myList[i] > max) {
+                max = myList[i];
+            }
         }
         System.out.println("Наибольший элемент: " + max);
     }
