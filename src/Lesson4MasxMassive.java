2ffcdaedcdc3344138d5ab742e5be31a9dd2a5b8
Nikolai-Shims <Nikolai.shims@gmail.com>
2020-08-26 21:11:09 +0300

    rand_with x2 massive

---
+++ src/Lesson4MasxMassive.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson4MasxMassive {
+
+    public static void main(String[] args) {
+        System.out.println("1й массив (длину)");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("2й массив (ширину)");
+        int b = sc.nextInt();
+        int min = 0;
+        int max = 10;
+        int diff = max - min;
+        Random random = new Random();
+
+        int[][] myList = new int[a][b];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+
+                myList[i][j] = random.nextInt(diff - 1);
+                System.out.print(myList[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
+
